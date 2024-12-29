a4ca1e25209bced6bfe9ba652107129a404ba9e3
DESKTOP-9BD52QQ\LARIN <daniel_larin77@outlook.com>
2024-12-28 20:53:15 -0600

    Se agrego configuración para convertir el microservicio en cliente eureka

--- src/main/java/com/debuggeandoideas/companies_crud/CompaniesCrudApplication.java
+++ src/main/java/com/debuggeandoideas/companies_crud/CompaniesCrudApplication.java
@@ -2,9 +2,11 @@ package com.debuggeandoideas.companies_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.debuggeandoideas.companies_crud")
 public class CompaniesCrudApplication {
 
